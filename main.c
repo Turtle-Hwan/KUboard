4c608c814e020f4c8b166088111f66c96f2ca0f3
Youngbin Ko <66235935+youngbin0417@users.noreply.github.com>
2021-05-25 15:15:25 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,25 @@
 #include <stdio.h>
+#include <string.h>
+#define MAX 30;
+
+bool correct(const char* que) {
+	char arr[MAX];
+	gets_s(arr, MAX);
+	int a = strcmp(que,arr);
+	if ( a == 0) {
+		return 1; // 두문자가 일치할 때
+	}
+	else {
+		return 0;
+	}
+}
+
+void score_save(const char* name, const int score) {
+	FILE* fp = fopen("점수.txt", "a");
+	fputs(name, fp);
+	fprintf(fp, " %d", score);
+	fputs("\n", fp);
+}
 
 int main() {
 	printf("hello world");
