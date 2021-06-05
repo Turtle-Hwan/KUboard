665f1a2ef2bd431875bf9575987a13777a4b24e2
kimheoynji <69509652+kimheoynji@users.noreply.github.com>
2021-06-06 08:41:05 +0900

    Update item.c

--- item.c
+++ item.c
@@ -1,12 +1,10 @@
 #include "header.h"
 
 void ITEM() {
-	gotoxy(CONSOLE_X / 2 + 60, CONSOLE_Y / 2 - 17);
-	color(" < ¾ÆÀÌÅÛ >",5);
+	gotoxy(CONSOLE_X/2 + 60, CONSOLE_Y/2 - 17);
+	color(" < 아이템 >",5);
 	gotoxy(CONSOLE_X / 2 + 65, CONSOLE_Y / 2 - 15);
-	printf("¡Û");
-	gotoxy(CONSOLE_X / 2 + 60, CONSOLE_Y / 2 - 13);
-	printf("1 ´­·¯ »ç¿ë!!");
+	printf("○");
 }
 
 
